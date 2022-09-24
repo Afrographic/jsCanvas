6b1060e90252738428058af306046cc18c7296d3
unknown <bruneltesse@gmail.com>
2022-09-24 16:48:43 +0100

    First commit

---
+++ engine.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 400;
+canvas.height = 400;
+
+// ctx.fillStyle = 'red';
+// ctx.fillRect(20, 20, 150, 100)
+// ctx.fillStyle = 'blue';
+// ctx.fillRect(190, 20, 150, 100)
+
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = 'green';
+// ctx.strokeRect(100, 200, 150, 100);
+
+// ctx.clearRect(25, 25, 100, 100)
+
+// ctx.fillStyle = 'black';
+// ctx.font = '30px Arial';
+// ctx.fillText('I love her a lot', 10, 50)
+// ctx.font = '12px Arial';
+// ctx.fillText('I love her a lot',10,80)
